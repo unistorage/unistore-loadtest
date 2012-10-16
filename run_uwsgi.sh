8d5ddc0e00b07c6a449c534278a7422021ff2d75
Anton Romanovich <anthony.romanovich@gmail.com>
2012-10-16 13:23:11 +0600

    Fix

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 touch ./uwsgi.touch
-$VIRTUAL_ENV/bin/uwsgi --module=app --callable=app --socket=:3000 --touch-reload=./uwsgi.touch -H $VIRTUAL_ENV
+$VIRTUAL_ENV/bin/uwsgi --module=app --callable=app --socket=:3000 --touch-reload=./uwsgi.touch --daemonize ./uwsgi.log -H $VIRTUAL_ENV
