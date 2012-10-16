25e6994931eef42214b9ae5074f75f93878be0f2
Anton Romanovich <anthony.romanovich@gmail.com>
2012-10-16 14:15:16 +0600

    Disable uwsgi logging

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 touch ./uwsgi.touch
-$VIRTUAL_ENV/bin/uwsgi --module=app --callable=app --socket=:3000 --touch-reload=./uwsgi.touch --daemonize ./uwsgi.log --workers 30 -H $VIRTUAL_ENV
+$VIRTUAL_ENV/bin/uwsgi --module=app --callable=app --socket=:3000 --touch-reload=./uwsgi.touch --daemonize ./uwsgi.log --workers 30 --disable-logging -H $VIRTUAL_ENV
